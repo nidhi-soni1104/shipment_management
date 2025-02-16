f1f4d76da8fc1a2eacd12cbfcc6c9a768dc4fc40
Nidhi Soni <nidhisoni4411@gmail.com>
2025-02-16 19:27:04 +0530

    Updated models with assocation

--- app/models/delivery_partner.rb
+++ app/models/delivery_partner.rb
@@ -1,3 +1,4 @@
 class DeliveryPartner < ApplicationRecord
   belongs_to :user
+  has_many :shipments
 end
--- app/models/shipment.rb
+++ app/models/shipment.rb
@@ -1,4 +1,14 @@
 class Shipment < ApplicationRecord
   belongs_to :customer
-  belongs_to :delivery_partner
+  belongs_to :delivery_partner, optional: true
+
+  enum status: { pending: "pending", assigned: "assigned", in_transit: "in_transit", delivered: "delivered" }
+
+  after_initialize :set_default_status, if: :new_record?
+
+  private
+
+  def set_default_status
+    self.status ||= "pending"
+  end
 end
